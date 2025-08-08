7a5fab11267f442d48f690d312ef68850a6d3a80
Lucia Salinas Ballesteros <lusalinas@live.com.ar>
2025-08-08 12:44:48 +0200

    modificar tarea

--- JS/index.js
+++ JS/index.js
@@ -17,19 +17,14 @@ addNewTask("enviar link de pago");
 addNewTask("presupuestar");
 addNewTask("pedir pasaportes");
 
-//funcion que muestra la lista//
+//funcion que muestra la lista
 
 function showList() {
   console.log(TaskList);
 }
 showList();
 
-//funcion que muestra una tarea//
-TaskList.forEach(function (tareas, indice) {
-  console.log(`${indice} : ${tareas} `);
-});
-
-//borra tarea//
+//funcion que borra una tarea
 
 function deleteTask(a) {
   let posicionTask = TaskList.indexOf(a);
@@ -37,3 +32,25 @@ function deleteTask(a) {
   console.log(TaskList);
 }
 deleteTask("pedir pasaportes");
+
+function position(a) {
+  let posicionTask = TaskList.indexOf(a);
+  console.log(posicionTask);
+}
+position("llamar cliente");
+
+//funcion que muestra una tarea
+function showTask(position) {
+  console.log(position);
+}
+
+showTask("llamar cliente");
+
+//funcion modifica tarea
+
+function modify(a) {
+  let posicionTask = TaskList.indexOf(a);
+  let modifiedTask = TaskList.slice(posicionTask, 1);
+  console.log(modifiedTask);
+}
+modify("llamar cliente");
